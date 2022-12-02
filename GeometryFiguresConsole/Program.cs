8455fb0353118a7d5a530107c83b5176ada848bf
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2022-12-02 16:29:54 +0300

    добавил консольное приложение для тестов

---
+++ GeometryFiguresConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿using Figures.Implementation;
+
+Console.WriteLine("Triangle area and perimeter");
+Triangle triangle = new Triangle(8.2, 7.3, 9.2);
+Console.WriteLine("Area: " + triangle.GetArea());
+Console.WriteLine("Perimeter" + triangle.GetPerimeter());
+
+Console.WriteLine("Circle area and perimeter");
+Circle circle = new Circle(7.32);
+Console.WriteLine("Area: " + circle.GetArea());
+Console.WriteLine("Perimeter " + circle.GetPerimeter());
